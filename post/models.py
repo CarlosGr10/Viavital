df9420aeaffe9c83544b771551805433be6e43ab
Carlos Garcia <cgarcia@raloy.com.mx>
2023-07-19 13:32:18 -0600

    modulo post

--- post/models.py
+++ post/models.py
@@ -5,6 +5,8 @@ class PostModel(models.Model):
     title = models.CharField(max_length=250)
     content = models.TextField()
     photo = models.ImageField(upload_to='posts/photos')
+    summary = models.CharField(max_length=250)
+    author = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
--- post/views.py
+++ post/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from .models import PostModel
 
+
 def main_view(request):
-    return render(request,"start.html")
+    return render(request, "start.html")
+
 
 def list_view_post(request):
     the_list = PostModel.objects.all()
-    context = {'the_list': the_list}
+    recent_post = PostModel.objects.all().order_by('-created')[:5]
+    context = {'posts': the_list, 'recent_posts': recent_post}
     return render(request, "post.html", context)
 
+
 def about_view(request):
-    return render(request, "about.html")
+    recent_post = PostModel.objects.all().order_by('-created')[:10]
+    context = {'recent_posts': recent_post}
+    return render(request, "about.html", context)
